9d564edb19016e43f987e33f1395be5b40d53e85
Chelsea Li <chelseali2001@gmail.com>
2021-05-29 01:41:53 -0700

    Wrote third test case that failed (for question 2). Tests if a year divisible by 400 is a leap year

--- leapyear_test.py
+++ leapyear_test.py
@@ -8,5 +8,8 @@ class TestCase(unittest.TestCase):
     def test0(self):
         self.assertEqual(leapyear.lp(100), "is not a leap year")
 
+    def test1(self):
+        self.assertEqual(leapyear.lp(400), "is a leap year")
+
 if __name__ == '__main__':
     unittest.main()
