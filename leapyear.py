e41613e31c02291a8ae8c3fcbfe4b63afdaca807
Chelsea Li <chelseali2001@gmail.com>
2021-05-29 01:44:14 -0700

    Implemented the method that tests if a year divisible by 400 is a leap year

--- leapyear.py
+++ leapyear.py
@@ -1,6 +1,8 @@
 def lp(n):
-    if n % 4:
-        if n % 100:
+    if n % 4 == 0:
+        if n % 100 == 0 and n % 400 != 0:
             return "is not a leap year"
+        else:
+            return "is a leap year"
     else:
         return "is not a leap year"
